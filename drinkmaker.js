956e4bc762a730e0084aacf2b4ad0001c480f125
BcnCarlos <carlos.fung@me.com>
2022-02-06 16:43:58 +0100

    classes, methods and jest test completed

--- drinkmaker.js
+++ drinkmaker.js
@@ -3,38 +3,35 @@
 class Drinkmaker{
     constructor(string, num ){
         this.type = string;
-        this.sugar = num;
-        this.stick = 0;
-        
+        this.sugar = num || 0;
+        this.stick = 0;       
     }
-    
- // makeDrink function to filter through the order and send message to the console. 
 
-    makeDrink(){       
-        
-        if (this.sugar > 0) this.stick = 1; 
-        
-        switch (this.type){
-            
-            case "T":
-                console.log(`M: Making Tea with ${this.sugar} sugars and ${this.stick} stick`);
-                break;
-            case  "H":
-                console.log(`M: Making Chocolate with ${this.sugar} sugars and ${this.stick} stick`);
-                break;
-            case "C":
-                console.log(`M: Making Coffee with ${this.sugar} sugars and ${this.stick} stick`);
-                break;
-            default:
-                console.log("Please enter, 'T' for tea, 'C' for coffee, or 'H' for chocolate")
-        }
-  
+// Make drink method that returns message with the drink selected, sugar, and stick. Sends error 
+
+makedrink(){
+
+    if (this.sugar > 0) this.stick = 1; 
+
+    switch (this.type){
+
+        case "T":
+           return  `M: Making Tea with ${this.sugar} sugars and ${this.stick} stick` ;
+            break;
+        case  "H":
+            return `M: Making Chocolate with ${this.sugar} sugars and ${this.stick} stick`;
+            break;
+        case "C":
+            return `M: Making Coffee with ${this.sugar} sugars and ${this.stick} stick`;
+            break;
+        default:
+            return "M: Please enter, 'T' for tea, 'C' for coffee, or 'H' for chocolate";
     }
+  
+}
 
 }
 
-// Test of a new order of coffee with 5 sugars
-
-const Neworder = new Drinkmaker("C", 5)
-
-Neworder.makeDrink() //Expected  "M: Making Coffee with 5 sugars and 1 stick"
+if (typeof module !== 'undefined') {
+    module.exports = Drinkmaker;
+  }
---
+++ test/drinkmaker.test.js
@@ -0,0 +1,66 @@
+const Order = require( '../drinkmaker')
+
+// Create Order test to check the proper expected return 
+
+describe('Order', () => {
+
+  let drink;
+  
+  it('Should make a cup of Tea', () => {
+    
+    // Create drink tea wit 2 sugars and 1 stick
+    
+    drink = new Order("T",2);
+
+    let tea = drink.makedrink();
+
+    let output = "M: Making Tea with 2 sugars and 1 stick";
+
+    expect(tea).toBe(output);
+
+  });
+
+
+  it('Should make a cup of Coffee', () => {
+    
+    // Create drink Coffee with 1 sugar and 1 stick. 
+    
+    drink = new Order("C", 1);
+
+    let coffee = drink.makedrink();
+
+    let output = "M: Making Coffee with 1 sugars and 1 stick";
+
+    expect(coffee).toBe(output);
+
+  });
+
+  it('Should make a cup of Chocolate', () => {
+    
+    // Create drink Chocolate with 0 sugar and 0 stick. 
+    
+    drink = new Order("H" );
+
+    let chocolate = drink.makedrink();
+
+    let output = "M: Making Chocolate with 0 sugars and 0 stick";
+
+    expect(chocolate).toBe(output);
+
+  });
+
+  it('Should return messages if incorrect drink input', () => {
+    
+    // Create drink Chocolate with 0 sugar and 0 stick. 
+    
+    drink = new Order("a", 2 );
+
+    let incorrect = drink.makedrink();
+
+    let output = "M: Please enter, 'T' for tea, 'C' for coffee, or 'H' for chocolate";
+
+    expect(incorrect).toBe(output);
+
+  });
+
+})
